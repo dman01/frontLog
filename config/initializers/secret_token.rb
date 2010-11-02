6e8b02e5da74d241ba26ab3379ed464ecbe53828
Mark Randow <mark.randow@gmail.com>
2010-11-02 18:29:54 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontlog::Application.config.secret_token = 'f4f75cc84357beeebbf649fe9a7cd999f04429a39466d0367144f0ffc6ac2d30373999ba62221a6c020b4ce834303dbd922bf2fbd4964adfdcec1c73ce21b6ba'
